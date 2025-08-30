176e7495361d6d04806dd6be85fb90ccf6a6fa22
Roger C. Pao <3468574+rcpao@users.noreply.github.com>
2025-08-30 00:50:11 -0700

    Initial script

---
+++ install_cacert_root_certs_into_ubuntu.sh
@@ -0,0 +1,16 @@
+#!/bin/bash -vx
+
+sudo apt-get install -y ca-certificates
+wget --no-check-certificate https://www.cacert.org/certs/root_X0F.crt
+wget --no-check-certificate https://www.cacert.org/certs/CAcert_Class3Root_x14E228.crt
+sudo cp *.crt /usr/local/share/ca-certificates/
+sudo update-ca-certificates
+
+CACERTSFILE=/etc/ssl/certs/ca-certificates.crt
+# for CRTFILE in root_X0F.crt CAcert_Class3Root_x14E228.crt; do
+for CRTFILE in *.crt; do
+    CRTLINE2=$(sed -n '2p' $CRTFILE)
+    grep $CRTLINE2 $CACERTSFILE
+    RC=$?
+    [ $RC -ne 0 ] && echo "error $0:$LINENO: $CRTFILE was _not_ found in $CACERTSFILE"
+done
